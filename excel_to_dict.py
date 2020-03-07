015a1a0cdc18166ac3622d2276438a61496dafbf
Griffin Poole <griffinpoole5@gmail.com>
2020-03-07 16:46:05 -0500

    added manual pretty printing to output

--- excel_to_dict.py
+++ excel_to_dict.py
@@ -1,5 +1,14 @@
 import pandas
 
+
 travel_doc = pandas.read_excel('./cities.xlsx')
+cities = travel_doc.to_dict('records')
 
 print(travel_doc)
+print()
+print()
+for i in range(0, len(cities)):
+    print(str(i) + ':')
+    for key in cities[i]:
+        print("     " + key + ':' + str(cities[i][key]))
+    print()
